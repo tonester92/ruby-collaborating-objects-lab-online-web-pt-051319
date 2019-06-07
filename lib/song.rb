c9768b4034ca4175022ae43ea60dd6fc02e33a17
Antonio Blandin <antonio.blandin25@gmail.com>
2019-06-07 02:06:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,10 +5,6 @@ class Song
     @name = name
   end
 
-  def self.new_by_filename(filename)
-    artist_name, song_name, extra = filename.split(" - ")
-    song = self.new(song_name)
-    artist = Artist.find_or_create_by_name(artist_name)
-    artist.add_song(song)
-  end
+
+
 end
