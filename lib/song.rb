1516b833eb8c5c2f331d2bf50f99d2f2499af233
Antonio Blandin <antonio.blandin25@gmail.com>
2019-06-07 02:07:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,5 +12,10 @@ class Song
     song.artist_name = artist_name
     song
   end
-
+  
+  def artist_name=(name)
+    artist = Artist.find_or_create_by_name(name)
+    self.artist = artist
+    artist.add_song(self)
+  end
 end
