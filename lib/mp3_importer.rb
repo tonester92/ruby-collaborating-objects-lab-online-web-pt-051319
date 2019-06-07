6fdc3a92c576988c846a4082930a28fee6865029
Antonio Blandin <antonio.blandin25@gmail.com>
2019-06-07 02:05:03 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -4,6 +4,10 @@ class MP3Importer
   def initialize(path)
     @path = path
   end 
+  
+  def files
+    Dir[@path+"/*.mp3"].map {|file| file.split("/").last }
+  end
 
   def import
     @files.each {|file| Song.new_by_filename(file)}
